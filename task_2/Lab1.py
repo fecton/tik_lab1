7c83cb30866ee594d86e0f9d17ac4a5c8bde7be6
Ruslan2901 <109164923+Ruslan2901@users.noreply.github.com>
2022-09-30 11:06:30 +0300

    Edit my task

--- task_2/Lab1.py
+++ task_2/Lab1.py
@@ -1,62 +1,23 @@
-import random
-import string
-import codecs
-import pandas as pd
-
-phrase = ""
-length = 50
-i = 0
-lang = 0
-while lang < 4:
-    #ENG
-    if lang == 1 :
-        print('English language')
-        for i in range(0, 40):
-            letters_and_digits = string.ascii_letters + string.digits
-            time = ''.join(random.sample(letters_and_digits, length))
-            phrase += time
-
-    #UKR
-    if lang == 2:
-        print('Ukrainian language')
-        with codecs.open('ukr.txt', encoding='utf-8') as fin:
-            phrase = next(fin)
-
-    #rus
-    if lang == 3:
-        print('russian language')
-        with codecs.open('rus.txt', encoding='utf-8') as fin:
-            phrase = next(fin)
-
-    #Character count
-    quantity = {'Symbols': 0, 'Letters': 0, 'Numbers': 0}
-    for i in phrase:
-        if i.isalpha():
-            quantity['Letters'] += 1
-        elif i.isdigit():
-            quantity['Numbers'] += 1
-        else:
-            quantity['Symbols'] += 1
-    Total = quantity['Numbers'] + quantity['Letters'] + quantity['Symbols']
-    #Output
-    print(pd.Series(list(phrase)).value_counts())
-    print('Numbers: %d, Letters: %d, Symbols: %d' % ( quantity['Numbers'], quantity['Letters'], quantity['Symbols']))
-    print('Total: %d' % Total)
-    phrase = ''
-    lang += 1
-
-
 import requests
 from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/National_Aerospace_University_–_Kharkiv_Aviation_Institute'
 response = requests.get(url)
 take = BeautifulSoup(response.text, 'lxml')
-name = take.find_all('span', class_='mw-page-title-main')
-faculty = take.find_all('li', class_='marker')
+KHAI = take.find_all('div', class_='mw-parser-output')
 
-for quote in name:
-    print(quote.text)
-
-for quote in faculty:
-    print(faculty.text)
+for quote in KHAI:
+    wikipedia = quote.text
+quantity = {'Symbols': 0, 'Letters': 0, 'Numbers': 0}
+for i in wikipedia:
+    if i.isalpha():
+        quantity['Letters'] += 1
+    elif i.isdigit():
+        quantity['Numbers'] += 1
+    else:
+        quantity['Symbols'] += 1
+Total = quantity['Numbers'] + quantity['Letters'] + quantity['Symbols']
+#Output
+print('Numbers: %d, Letters: %d, Symbols: %d' % ( quantity['Numbers'], quantity['Letters'], quantity['Symbols']))
+print('Total: %d' % Total)
+print('The site is encoded in UTF-8 format, 1 character = 1 Byte => %d Byte or %d KB' % ( Total, round(Total / 1024)))
