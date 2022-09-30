995f35f69b308d0c7c34a59d64c55178ca1a897f
Ruslan2901 <109164923+Ruslan2901@users.noreply.github.com>
2022-09-30 12:17:40 +0300

    Added comments and friendly interface

--- task_2/Lab1.py
+++ task_2/Lab1.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Displaying information about the text
+print('Information about the university on wikipedia')
+print()
+
+#Set url
 url = 'https://en.wikipedia.org/wiki/National_Aerospace_University_–_Kharkiv_Aviation_Institute'
 response = requests.get(url)
 take = BeautifulSoup(response.text, 'lxml')
-KHAI = take.find_all('div', class_='mw-parser-output')
 
-for quote in KHAI:
+# Take data
+KhAI = take.find_all('div', class_='mw-parser-output')
+
+#Print
+for quote in KhAI:
     wikipedia = quote.text
+    print(wikipedia)
+
+#Data sorting
 quantity = {'Symbols': 0, 'Letters': 0, 'Numbers': 0}
 for i in wikipedia:
     if i.isalpha():
@@ -17,6 +28,7 @@ for i in wikipedia:
     else:
         quantity['Symbols'] += 1
 Total = quantity['Numbers'] + quantity['Letters'] + quantity['Symbols']
+
 #Output
 print('Numbers: %d, Letters: %d, Symbols: %d' % ( quantity['Numbers'], quantity['Letters'], quantity['Symbols']))
 print('Total: %d' % Total)
