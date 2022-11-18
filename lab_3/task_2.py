953437f68604298597299255c8ec1d10b6d253d5
Andrew Fecton <bopongamer@gmail.com>
2022-11-18 15:22:19 +0100

    beginning of writing task 2

--- lab_3/task_2.py
+++ lab_3/task_2.py
@@ -9,3 +9,14 @@ Task 2:
 Тривалості символів: r1 = 1c, r2 = 2c, rk = rk - 1 + 1
 Імовірність появи символів взяти з пункту 1.1
 """
+
+from .task_1 import *
+
+print("\n\tTask 2\n")
+
+MaxEntropy = log2(len(symbols))
+Entropy = InfoAmount_B
+UnderLoadedSymb = (MaxEntropy - Entropy) / Entropy
+
+print("UnderLoaded Symbols(b) = ", UnderLoadedSymb)
+
