218a1bc6fa176dbe6b00fecc4b293a81d9b83360
Andrew Fecton <bopongamer@gmail.com>
2022-11-27 17:35:29 +0100

    Co-authored-by: rus.ishkov.04@gmail.com <rus.ishkov.04@gmail.com> Co-authored-by: illiavoloshin128@gmail.com <illiavoloshin128@gmail.com>

--- lab_3/task_3.py
+++ lab_3/task_3.py
@@ -15,34 +15,148 @@ Task 3:
 Використовувати будь-які пристої для кодування та декодування
 """
 
-from func import *
+# Modules
+# =============================================
+import math
+import collections
+# =============================================
+
+# Global variables
+# =============================================
+TextToEncode = ""
+TextToDecode = ""
+Text = ""
+
+SDI                         = dict()
+codingTable                 = dict()
+arrSortedKeys               = []
+arrDefaultVer               = []
+
+entropyInput                = 0.0
+averageCountBitsPerSymbol   = 0.0
+closeCoef                   = 0.0
+needModeof                  = False
+# =============================================
+
+# Fuctions
+# =============================================
+def menu() -> None:
+    print(""" -= Menu =-
+0. Menu
+1. Encode
+2. Decode
+3. Exit""")
 
 
-def main():
-    message = input("Enter your message: ")
-    # message = "ferfioergjirejigeio"
-    # Кількість повторень символів
-    sizes = size_caulculation(message)
-    print("Повідомлення: %s" % message)
+# =============================================
 
-    print("Кількість повторень")
-    for key in sizes.keys():
-        print("%s : %s" % (key, sizes[key]))
+# Main program cycle
+# =============================================
+class Program:
 
-    print("Можливість появи")
-    prob = probability(message)
-    for i in prob:
-        print(round(i, 5), end=" ")
-    print()
-    print("Ентропія: %f" % Entropy(prob))
-    print("Відcортований за спаданням можливість мояви") 
-    prob = sorted(prob)
-    for i in prob:
-        print(round(i, 5), end=" ")
-    print()
+    def main():
+        Program.DataIntializationInputText()
+        Program.CalculatePossibility()
+        Program.GlobalScript()
+        Program.CodingText()
+        Program.SetDGVSDI()
 
-    ShannonFanoRec(message)
 
-op = 0
-while 1:
-    main()
+    @classmethod
+    def EnterData():
+        global Text
+
+        Text = input("[TEXT] >> ")
+
+    @classmethod
+    def DataIntializationInputText():
+        global SDI, codingTable
+
+        SDI         = dict()
+        codingTable = dict()
+
+
+    @classmethod
+    def CalculatePossibility():
+        global Text
+
+        for el in Text:
+            if not el in SDI:
+                SDI[el] = Text.count(el) / len(Text)
+            if not codingTable.get(el, 0):
+                codingTable[el] = ""
+
+    @classmethod
+    def GlobalScript():
+        raise NotImplementedError
+    
+
+    @classmethod
+    def CodingText():
+        raise NotImplementedError
+
+
+    @classmethod
+    def SetDGVSDI():
+        raise NotImplementedError
+
+
+# =============================================
+
+# Additional classes
+# =============================================
+
+class GlobalScript:
+    @classmethod
+    def Main():
+        GlobalScript.SortSDIAndCalculateEntropy()
+        if not needModeof:
+            arrDefaultVer = SDI
+        arrSortedKeys = SDI.keys()
+        GlobalScript.ShenonFanoMainFunc(0, len(SDI.keys()))
+        GlobalScript.CalculateAverageCountBitsPerSymbol()
+        GlobalScript.AnalizeGettedCountAndInputEntropy()
+        Program.CodingText()
+        Program.SetDGVSDI()
+
+    @classmethod
+    def ShenonFanoMainFunc(L: int, R: int):
+        raise NotImplementedError
+
+    @classmethod
+    def CalculateAverageCountBitsPerSymbol():
+        raise NotImplementedError
+
+    @classmethod
+    def AnalizeGettedCountAndInputEntropy():
+        raise NotImplementedError
+
+
+# =============================================
+
+
+def work_with_text( text: str ) -> None:
+    text = text.lower()
+    I = 0
+    symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', '/', '*', '!', '@', '#', '$', '%', '^', '&', '?', '(', ')', '[', ']', '{', '}', ' ']
+    #Sorting
+    for i in symbols:
+        c = collections.Counter()
+        for symbol in text:
+             c[symbol] += 1
+    Total = sum(c.values())
+    #Shannon's formula
+    for i in symbols:
+        for symbol in text:
+            n = c[i]
+        if n > 0:   
+            #print(i, c[i]) # Number of certain characters
+            p = n / Total # Probability
+            I += math.log2(1/p) # Entropy
+            # За необхідністю зробити вивід p або I
+    pass
+    
+
+
+
+
